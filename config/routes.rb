0195731e19efad3e281b2c5e63229b573ecf1910
Slava Protashchik <v.protashchik@syberry.com>
2015-06-24 11:08:26 +0300

    Finish loyout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root  'static_page#home'
+  match '/signup',  to: 'users#new',           via: 'get'
   match '/help',    to: 'static_page#help',    via: 'get'
   match '/about',   to: 'static_page#about',   via: 'get'
   match '/contact', to: 'static_page#contact', via: 'get'
